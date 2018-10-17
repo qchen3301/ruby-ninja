3a803c502df9d2909c939843d44caef4f94c78c1
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-17 16:54:02 -0400

    integers skills

--- ruby-ninja.rb
+++ ruby-ninja.rb
@@ -17,7 +17,15 @@ puts true_statement
 def run_my_life
     puts @variable_my_life 
     puts "#{@variable_my_life} was here"
-    
+    puts "#{@variable_my_life.class} was here"
+
 end
 
 puts run_my_life
+
+@variable_d = 3301
+puts "variable_d equals #{@variable_d} "
+@variable_e = 330.1
+puts "variable_e equals #{@variable_e}"
+@variable_d = @variable_e
+puts "variable_d now equals #{@variable_d}"
